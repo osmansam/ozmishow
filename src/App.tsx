89bd977346106f60ce5f34b07b8ba59fc930d0de
Osman <114867365+osmansam@users.noreply.github.com>
2023-04-21 16:20:33 -0400

    body is set

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,10 @@ import Press from "./scenes/press";
 function App() {
   return (
     <div className="App">
-      {/* <video autoPlay loop muted id="video" className="fixed z-[-1] w-full  ">
-        <source
-          src="https://www.ultimacollection.com/application/files/3016/1668/4590/cover.mp4"
-          type="video/mp4"
-        />
-      </video> */}
-      {/* <Navbar /> */}
+      <Navbar />
       <Home />
-      <Services />
       <Collections />
+      <Services />
       <Press />
     </div>
   );
