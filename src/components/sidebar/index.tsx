cd307cf748051c11ae1aca911c2cf2dba1cb6609
Osman <114867365+osmansam@users.noreply.github.com>
2023-07-02 10:09:25 -0400

    sidebar is rearranged

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState, useAppDispatch } from "../../store";
 import { setIsSidebarOpen } from "../../features/context/contextSlice";
 import { getNavbar } from "../../features/twoPicture/twoPictureSlice";
 import { useNavigate } from "react-router-dom";
-import { LanguageOptions } from "../../shared/types";
+import { LanguageOptions, PageOptionsType } from "../../shared/types";
 
-type Props = { currentPage: string };
+type Props = {
+  currentPage: string;
+};
 
 const Sidebar = ({ currentPage }: Props) => {
   const navigate = useNavigate();
@@ -20,6 +21,8 @@ const Sidebar = ({ currentPage }: Props) => {
     (state: RootState) => state.twoPicture
   );
   const { language } = useSelector((state: RootState) => state.context);
+
+  const [activeMotherPage, setActiveMotherPage] = useState<string>("");
 
   useEffect(() => {
     const handleResize = () => {
@@ -35,24 +38,89 @@ const Sidebar = ({ currentPage }: Props) => {
       window.removeEventListener("resize", handleResize);
     };
   }, [dispatch]);
-  //   get the logo
+
+  // Get subpages for a given mother page
+  const getSubpages = (motherPage: string): PageOptionsType[] => {
+    return pageOptions.filter(
+      (item) =>
+        item.motherPageEN === motherPage || item.motherPageTR === motherPage
+    );
+  };
+
+  // Render subpages recursively
+  const renderSubpages = (subpages: PageOptionsType[]) => {
+    return subpages.map((subpage, index) => (
+      <li
+        key={index}
+        className={`pl-8 py-2 m-2 text-white cursor-pointer hover:underline ${
+          currentPage === subpage.pageNameEN &&
+          "bg-[#9f000f] text-black rounded-md hover:no-underline"
+        }`}
+        onClick={() => navigate(`/${subpage.pageNameEN}`)}
+      >
+        {language === LanguageOptions.EN
+          ? subpage.pageNameEN
+          : subpage.pageNameTR}
+      </li>
+    ));
+  };
+
+  // Render navigation links
+  const renderNavigationLinks = () => {
+    return pageOptions.map((page, index) => {
+      if (page.isNavbar) {
+        const subpages = getSubpages(page.pageNameEN);
+        const isActiveMotherPage = activeMotherPage === page.pageNameEN;
+        return (
+          <React.Fragment key={index}>
+            <li
+              className={`pl-4 py-2 m-2 text-white uppercase cursor-pointer hover:underline ${
+                currentPage === page.pageNameEN &&
+                "bg-[#9f000f] text-black rounded-md hover:no-underline"
+              }`}
+              onClick={() => {
+                if (subpages.length > 0) {
+                  setActiveMotherPage(
+                    isActiveMotherPage ? "" : page.pageNameEN
+                  );
+                } else {
+                  navigate(`/${page.pageNameEN}`);
+                }
+              }}
+            >
+              {language === LanguageOptions.EN
+                ? page.pageNameEN
+                : page.pageNameTR}
+            </li>
+            {isActiveMotherPage && (
+              <ul className="pl-4">{renderSubpages(subpages)}</ul>
+            )}
+          </React.Fragment>
+        );
+      } else {
+        return null;
+      }
+    });
+  };
+
   useEffect(() => {
     dispatch(getNavbar());
   }, [dispatch]);
+
   return (
     <div className="z-40">
-      <div className={`${isSidebarOpen ? "" : "hidden"}  z-30 `}>
+      <div className={`${isSidebarOpen ? "" : "hidden"}  z-30`}>
         <div
           className={`${
             isSidebarOpen ? "" : "hidden"
-          } lg:hidden fixed top-0  w-full h-full z-10`}
+          } lg:hidden fixed top-0 w-full h-full z-10`}
           onClick={() => dispatch(setIsSidebarOpen(false))}
         ></div>
-        <div className="w-[250px] lg:w-1/6 h-screen bg-[#343a3b] fixed flex flex-col z-50 ">
+        <div className="w-[250px] lg:w-1/6 h-screen bg-[#343a3b] fixed flex flex-col z-50">
           {/* image  */}
-          <div className="mx-auto ">
+          <div className="mx-auto">
             <img
-              className="py-2 cursor-pointer rounded-xl h-36 bg-[#343a3b] "
+              className="py-2 cursor-pointer rounded-xl h-36 bg-[#343a3b]"
               src={logo ? logo : "https://via.placeholder.com/150"}
               alt="logo"
               onClick={() => {
@@ -60,25 +128,8 @@ const Sidebar = ({ currentPage }: Props) => {
               }}
             />
           </div>
-          <ul className="w-3/4  my-auto flex flex-col justify-between mx-auto  pt-4 overflow-y-auto no-scrollbar">
-            {" "}
-            {pageOptions.map(
-              (page, index) =>
-                page.isNavbar && (
-                  <li
-                    key={index}
-                    className={` pl-4 py-2 m-2  text-white uppercase cursor-pointer hover:underline ${
-                      currentPage === page.pageNameEN &&
-                      "bg-[#9f000f] text-black rounded-md hover:no-underline"
-                    }`}
-                    onClick={() => navigate(`/${page.pageNameEN}`)}
-                  >
-                    {language === LanguageOptions.EN
-                      ? page.pageNameEN
-                      : page.pageNameTR}
-                  </li>
-                )
-            )}
+          <ul className="w-3/4 my-auto flex flex-col justify-between mx-auto pt-4 overflow-y-auto no-scrollbar">
+            {renderNavigationLinks()}
           </ul>
         </div>
       </div>
