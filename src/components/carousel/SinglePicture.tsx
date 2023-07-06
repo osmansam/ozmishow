deb8cdfaa253fb3225d16b134232db995f785601
Osman <114867365+osmansam@users.noreply.github.com>
2023-07-06 15:59:39 -0400

    carousel padding

--- src/components/carousel/SinglePicture.tsx
+++ src/components/carousel/SinglePicture.tsx
@@ -5,17 +5,17 @@ type Props = {
 };
 
 const SinglePicture = ({ header, paragraphs, img }: Props) => {
-  const overlayStyles = `p-5 absolute  flex
-  h-[380px] w-[450px] z-30 flex-col items-center justify-center
-  whitespace-normal bg-[#FF6B66] text-center text-white
-  opacity-0 transition duration-500 hover:opacity-90`;
-
   return (
     <li className="relative mx-5 inline-block h-[380px] w-[450px] ">
-      <div className={overlayStyles}>
+      <div
+        className="p-5 absolute  flex
+  h-[380px]  w-[450px] z-30 flex-col items-center justify-center
+  whitespace-normal bg-[#FF6B66] text-center text-white
+  opacity-0 transition duration-500 hover:opacity-90"
+      >
         <p className="text-2xl">{header}</p>
         {paragraphs?.map((paragraph, index) => (
-          <p key={index} className=" font-[400] leading-6">
+          <p key={index} className=" font-[400] leading-6 px-2">
             {paragraph}
           </p>
         ))}
