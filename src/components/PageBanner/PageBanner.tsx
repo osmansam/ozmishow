1b4be818e92c15997ad460add5171e7cfba2b7aa
Osman <114867365+osmansam@users.noreply.github.com>
2023-09-12 15:01:28 -0500

    Page banner cleared

--- src/components/PageBanner/PageBanner.tsx
+++ src/components/PageBanner/PageBanner.tsx
@@ -1,38 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { PictureType, PictureWithStyleType } from "../../shared/types";
-import StyledModal from "../../hooks/styledModal/StyledModal";
-import ContentModal from "../../hooks/contentModal/ContentModal";
-import { useSelector } from "react-redux";
-import { RootState, useAppDispatch } from "../../store";
-import { AiOutlineDown } from "react-icons/ai";
-import { style } from "../../shared/types";
-
-type Props = {};
+import { PictureWithStyleType } from "../../shared/types";
+import StyleModalContainer from "../../hooks/styledModal/StyleModalContainer";
 
 const PageBanner = ({ img, header, _id }: PictureWithStyleType) => {
-  const dispatch = useAppDispatch();
-  const { isAdmin } = useSelector((state: RootState) => state.context);
-  const [isModalOpen, setIsModalOpen] = useState(false); // State to control modal visibility
-  const [isContentModalOpen, setIsContentModalOpen] = useState(false);
-  const [contentToEdit, setContentToEdit] = useState<any>();
-  const [contentType, setContentType] = useState("");
-  const [contentModalContentType, setContentModalContentType] = useState("");
-
-  const [selectedStyle, setSelectedStyle] = useState({
-    content: "",
-    style: style,
-  });
-
-  const openModal = (styleData: any) => {
-    setSelectedStyle(styleData);
-    setIsModalOpen(true);
-  };
-  const openContentModal = (content: any, contentType: string) => {
-    setContentToEdit(content);
-    setContentModalContentType(contentType);
-
-    setIsContentModalOpen(true);
-  };
   return (
     <div className="relative w-full pt-10 mb-10">
       {/* image */}
@@ -41,34 +10,17 @@ const PageBanner = ({ img, header, _id }: PictureWithStyleType) => {
       <div className="absolute bottom-[-20px] left-20  bg-[#9f000f] text-white">
         <h2
           className="text-xl uppercase px-4 py-2 font-[700] flex flex-row gap-2"
-          style={header?.style ? header?.style : {}}
+          style={header?.style}
         >
           {header?.content}
-          {!isModalOpen && isAdmin && (
-            <AiOutlineDown
-              className="text-lg justify-end my-auto"
-              onClick={() => {
-                openModal({
-                  style: header?.style,
-                  content: header?.content,
-                });
-                setContentType("header");
-              }}
-            />
-          )}
-          {isModalOpen && contentType === "header" && (
-            <StyledModal
-              key={_id}
-              isOpen={isModalOpen}
-              styleData={selectedStyle}
-              onClose={() => setIsModalOpen(false)}
-              type="twoPicture"
-              twoPictureId={_id ?? ""}
-              componentId={""}
-              contentType="header"
-              isContentSend={true}
-            />
-          )}
+          <StyleModalContainer
+            styleData={header}
+            twoPictureId={_id ?? ""}
+            componentId={""}
+            contentContainerType="header"
+            isContentSend={true}
+            type="twoPicture"
+          />
         </h2>
       </div>
     </div>
